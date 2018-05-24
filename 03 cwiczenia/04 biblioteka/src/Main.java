4b47b3f003600f824ab53d21c8a924798bc43d33
Grzegorz Borowik <borowik.grzegorz@gmail.com>
2018-05-24 17:17:10 +0200

    04 biblioteka update

--- 03 cwiczenia/04 biblioteka/src/Main.java
+++ 03 cwiczenia/04 biblioteka/src/Main.java
@@ -1,6 +1,33 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        String csvFile = "biblioteka.csv";
+        String line = "";
+        String cvsSplitBy = ";";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+
+            while ((line = br.readLine()) != null) {
+
+                String[] linia = line.split(cvsSplitBy);
+
+                System.out.println("autor: " + linia[0]
+                        + "tytul: " + linia[1]
+                        + "wydawca: " + linia[2]
+                        + "rok: " + linia[3]
+                        + "ISBN: " + linia[4]
+                        + "liczba stron: " + linia[5]
+                );
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
