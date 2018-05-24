b956735aeab7ac5aa71765487cbdf2566639042b
Grzegorz Borowik <borowik.grzegorz@gmail.com>
2018-05-24 15:51:04 +0200

    03 Auto

---
+++ 03 cwiczenia/03 auto/src/Auto.java
@@ -0,0 +1,99 @@
+import static java.lang.System.out;
+
+public class Auto {
+    String nazwa;
+    String typ;
+    double pojSilnik;
+    double pojBaku;
+    int rokProdukcji;
+
+    Auto(String nazwa, String typ, double pojSilnik,
+         double pojBaku, int rokProdukcji) {
+        this.nazwa = nazwa;
+        this.typ = typ;
+        this.pojSilnik = pojSilnik;
+        this.pojBaku = pojBaku;
+        this.rokProdukcji = rokProdukcji;
+    }
+
+    boolean jestWlaczony;
+
+    boolean wlaczSilnik() {
+        if (jestWlaczony == false) {
+            jestWlaczony = true;
+            predkosc = 0;
+        } else {
+            out.println("Silnik juz jest wlaczony!");
+        }
+        return jestWlaczony;
+    }
+
+
+    boolean czyWlaczony() {
+        return jestWlaczony;
+    }
+
+    int ktoryWlasciciel = 1;
+
+    int sprzedaj() {
+        return ++ktoryWlasciciel;
+    }
+
+    double stanPaliwa = 0.0;
+
+    double tankuj(double dolewka) {
+        if (dolewka > 0.0
+                && pojBaku - stanPaliwa >= dolewka) {
+            //stanPaliwa = stanPaliwa + dolewka;
+            stanPaliwa += dolewka;
+        } else {
+            out.println(
+                    "Nie odpowiednia ilosc dolewki"
+            );
+        }
+        return stanPaliwa;
+    }
+
+    int predkosc;
+
+    int zwiekszPredkoscO_10() {
+        predkosc += 10;
+        return predkosc;
+    }
+
+    int zmniejszPredkoscO_10() {
+        if (predkosc >= 10) {
+            predkosc -= 10;
+        } else {
+            out.println("Zbyt mała prędkość");
+        }
+        return predkosc;
+    }
+
+    void wyswietlInfo() {
+        out.println("Nazwa: " + nazwa);
+        out.println("Typ pojazdu: " + typ);
+        out.println("Pojemnosc silnika: " + pojSilnik);
+        out.println("Pojemnosc baku: " + pojBaku);
+        out.println("Rok produkcji: " + rokProdukcji);
+        out.println("Stan paliwa: " + stanPaliwa);
+        out.println("Wlasciciel nr: " + ktoryWlasciciel);
+        out.println("Predkosc: " + predkosc);
+        out.println("Włączony: " + czyWlaczony());
+    }
+
+    public static void main(String[] arguments) {
+        Auto auto = new Auto("Syrena Bosto", "Van", 1.0, 20.0, 1970);
+
+        auto.sprzedaj();
+
+        auto.tankuj(10.0);
+        auto.wlaczSilnik();
+        auto.zwiekszPredkoscO_10();
+        auto.zwiekszPredkoscO_10();
+
+        auto.wyswietlInfo();
+
+    }
+
+}
