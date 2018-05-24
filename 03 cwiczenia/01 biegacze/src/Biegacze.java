eeceb13846377d3eed1dfa11ba90ff79f261ffd5
Grzegorz Borowik <borowik.grzegorz@gmail.com>
2018-05-24 10:34:25 +0200

    comment update

--- 03 cwiczenia/01 biegacze/src/Biegacze.java
+++ 03 cwiczenia/01 biegacze/src/Biegacze.java
@@ -3,7 +3,8 @@ public class Biegacze {
         int minValue = Integer.MAX_VALUE;
         int minIndex = -1;
         for (int i = 0; i < values.length; i++)
-            if (values[i] < minValue) {
+            if (values[i] < minValue) { // '<' ostra nierówność = pierewszy indeks najnmiejszego el
+                                        // '<=' słaba nierówność = ostatni indeks najmniejszego el
                 minValue = values[i];
                 minIndex = i;
             }
@@ -18,13 +19,15 @@ public class Biegacze {
                 continue;
             if (secondIdx == -1 ||
                     values[i] < values[secondIdx])
+                // '<' ostra nierówność = pierewszy indeks najnmiejszego el
+                // '<=' słaba nierówność = ostatni indeks najmniejszego el
                 secondIdx = i;
         }
         return secondIdx;
     }
 
     public static void main(String[] args) {
-        int[] czasy = {0};
+        int[] czasy = {3, 3, 1, 5, 2, 7, 3, 3, 3, 3};
 
         System.out.println("Indeks pierwszego biegacza: " + getMinIndex(czasy));
         System.out.println("Indeks drugiego biegacza: " + getSecondMinIndex(czasy));
