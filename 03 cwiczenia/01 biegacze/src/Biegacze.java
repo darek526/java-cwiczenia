cbcf624f92f1042ad96a88f12d0b347be4280526
Grzegorz Borowik <borowik.grzegorz@gmail.com>
2018-05-24 10:24:43 +0200

    cwiczenia 03

---
+++ 03 cwiczenia/01 biegacze/src/Biegacze.java
@@ -0,0 +1,33 @@
+public class Biegacze {
+    public static int getMinIndex(int[] values) {
+        int minValue = Integer.MAX_VALUE;
+        int minIndex = -1;
+        for (int i = 0; i < values.length; i++)
+            if (values[i] < minValue) {
+                minValue = values[i];
+                minIndex = i;
+            }
+        return minIndex;
+    }
+
+    public static int getSecondMinIndex(int[] values) {
+        int secondIdx = -1;
+        int minIdx = getMinIndex(values);
+        for (int i = 0; i < values.length; i++) {
+            if (i == minIdx)
+                continue;
+            if (secondIdx == -1 ||
+                    values[i] < values[secondIdx])
+                secondIdx = i;
+        }
+        return secondIdx;
+    }
+
+    public static void main(String[] args) {
+        int[] czasy = {0};
+
+        System.out.println("Indeks pierwszego biegacza: " + getMinIndex(czasy));
+        System.out.println("Indeks drugiego biegacza: " + getSecondMinIndex(czasy));
+
+    }
+}
